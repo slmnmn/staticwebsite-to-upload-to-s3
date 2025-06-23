eb92f0e1e65b066defae52bbf14e1e57e2c00d13
Salmon <example@gmail.com>
2025-06-23 14:51:57 -0500

    holabuenosdias

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component{
   const filename = file.name;
 
   // URL DINAMICA PARA PODER CAMBIARLA CUALQUIER COSA.
-  const apiUrl = `https://w3u5fc7bha.execute-api.us-east-1.amazonaws.com/prod/bucket-prueba-despliegue-imagenes-full-localv5/${encodeURIComponent(filename)}`;
+  const apiUrl = `https://j40pms8087.execute-api.us-east-1.amazonaws.com/prod/bucket-prueba-despliegue-imagenes-full-localv5/${encodeURIComponent(filename)}`;
 
   console.log(`Sending file to: ${apiUrl}`);
 
