aa858acd7973d7986e683740ff04f09ebd735587
Salmon <example@gmail.com>
2025-06-23 23:35:38 -0500

    DeployWebsite Test 1

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component{
   const filename = file.name;
 
   // URL DINAMICA PARA PODER CAMBIARLA CUALQUIER COSA.
-  const apiUrl = `https://j40pms8087.execute-api.us-east-1.amazonaws.com/prod/bucket-prueba-despliegue-imagenes-full-localv5/${encodeURIComponent(filename)}`;
+  const apiUrl = `https://h41d72db3k.execute-api.us-east-1.amazonaws.com/prod/bucket-prueba-despliegue-imagenes-terraform-githubactions/${encodeURIComponent(filename)}`;
 
   console.log(`Sending file to: ${apiUrl}`);
 
